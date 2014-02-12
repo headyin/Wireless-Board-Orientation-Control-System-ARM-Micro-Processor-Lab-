0dc15bcaf8f668d6f0303e192c2372cf30d4f129
Xinshang Yin <xinshang.yin@mail.mcgill.ca>
2014-02-12 17:16:23 -0500

    7-seg displays starting

---
+++ lab2/src/sseg_display.c
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file    sseg_display.h
+  * @author  Xinshang Yin, Chandani Patel
+  * @version V1.0.0
+  * @date    06-February-2014
+  * @brief   This file provides functions to show a 3-digit number through 3 7-segement displays 
+  */ 
+	
+/* Includes ------------------------------------------------------------------*/
+#include "sseg_display.h"
+
+
+/* Exported defines -------------------------------------------------------- */
+
+
+
+/* Exported variables ------------------------------------------------------------*/ 
+
+
+/* Exported functions --------------------------------------------------------------*/
+
+/**
+  * @brief  Initialize GPIOA pin0 as PWG output (TIM5 CH1)
+  * @param  None
+  * @retval None
+  */
+
+
---
+++ lab2/src/sseg_display.h
@@ -0,0 +1,19 @@
+/**
+  ******************************************************************************
+  * @file    sseg_display.c
+  * @author  Xinshang Yin, Chandani Patel
+  * @version V1.0.0
+  * @date    06-February-2014
+  * @brief   This file provides functions to show a 3-digit number through 3 7-segement displays 
+  */ 
+	
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+
+/* Private defines -------------------------------------------------------- */
+
+
+
+/* Private variables ------------------------------------------------------------*/ 
+
