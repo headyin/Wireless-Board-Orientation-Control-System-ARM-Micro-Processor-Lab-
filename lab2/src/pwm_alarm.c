84794029b1786f0b2989d2b1501356be5dd6b42d
Xinshang Yin <xinshang.yin@mail.mcgill.ca>
2014-02-12 13:38:14 -0500

    use PD14 TIM4 CH3 Red Led for the alarm

--- lab2/src/pwm_alarm.c
+++ lab2/src/pwm_alarm.c
@@ -32,16 +32,16 @@ void pwm_alarm_init(void)
 
 void alarm_gpio_init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitTypeDef GPIO_InitStructure;
 
-  /* TIM5 clock enable */
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  /* TIM4 clock enable */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 
-  /* GPIOA clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  /* GPIOD clock enable */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
   
-  /* GPIOC Configuration: TIM5 CH1 (PA0) */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  /* GPIOC Configuration: TIM4 CH3 (PD14) */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
 	/* GPIO Alternate function Mode */
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 	/* High speed */
@@ -50,10 +50,10 @@ void alarm_gpio_init(void)
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	/* Pull up */
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-  GPIO_Init(GPIOA, &GPIO_InitStructure); 
+  GPIO_Init(GPIOD, &GPIO_InitStructure); 
 
-  /* Connect TIM5 pins to AF2 */  
-  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_TIM5);
+  /* Connect TIM4 pins to AF2 */  
+  GPIO_PinAFConfig(GPIOD, GPIO_PinSource0, GPIO_AF_TIM4);
 }
 
 void alarm_tim_init(void)
@@ -66,21 +66,21 @@ void alarm_tim_init(void)
   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
 	
-	TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
 	
 	TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = 50;
   TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
 
-  TIM_OC1Init(TIM5, &TIM_OCInitStructure);
+  TIM_OC1Init(TIM4, &TIM_OCInitStructure);
 
-  TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable);
+  TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
 	
-	TIM_ARRPreloadConfig(TIM3, ENABLE);
+	TIM_ARRPreloadConfig(TIM4, ENABLE);
 	
-	/* TIM3 enable counter */
-  TIM_Cmd(TIM3, ENABLE);
+	/* TIM4 enable counter */
+  TIM_Cmd(TIM4, ENABLE);
 	
 	
 }
