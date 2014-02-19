4dd2fbbb33010aa2a4e8f3ff76484e33d40f14d1
ChandaniP <chanipatel3@gmail.com>
2014-02-19 15:29:48 -0500

    lab3 base project

---
+++ Lab3/src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "math.h"
+#include "atan_LUT.h"
+
+//NOTE TO STUDENTS: Uncomment the version of drivers related to your sensor
+//#include "lis3dsh.h"
+//#include "stm32f4_discovery_lis302dl.h"
+
+int main()
+{
+	
+}
+
+
+
+
+
+
+
